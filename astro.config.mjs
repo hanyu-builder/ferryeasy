fde0b396b7ec3a7b07332087f56f082b49419d2c
hanyu-builder <rakujp17@gmail.com>
2025-12-24 15:30:16 +0800

    update footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   // 🟢 請改成你的新網域
   site: 'https://ferryeasy.app', 
-  
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
