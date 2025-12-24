cf3e0df1bb260ab69229de94f6b0bfc2f6d8bd03
hanyu-builder <rakujp17@gmail.com>
2025-12-24 13:05:24 +0800

    add PWA support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  // 🟢 請改成你的新網域
+  site: 'https://ferryeasy.app', 
+  
+  integrations: [tailwind()]
 });
